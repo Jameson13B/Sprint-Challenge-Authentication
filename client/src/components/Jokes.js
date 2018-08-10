b5bd4a6adefcdb9bd08c7ee34e5ea606f050ba89
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-10 11:26:02 -0600

    Created jokes component and functionality to collect data, store in state, and render on component

---
+++ client/src/components/Jokes.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: []
+    }
+  }
+  componentDidMount() {
+    const token = localStorage.getItem('jwt');
+    const requestOptions = {
+      headers: {
+        Authorization: token,
+      },
+    };
+    axios.get('http://localhost:5000/api/jokes', requestOptions)
+      .then(response => {
+        this.setState({ jokes: response.data });
+      })
+      .catch(err => {
+        console.error('Axios Failed');
+      })
+  }
+  handleLogout = e => {
+    localStorage.removeItem('jwt')
+    this.props.history.push('/signin');
+  }
+  render() {
+    return (
+      <div className='jokes'>
+        {/* Logout Button */}
+        {localStorage.getItem('jwt') && (
+          <div className='btn' onClick={this.handleLogout}>Logout</div>
+        )}
+        <div className='jokes-list'>
+          {this.state.jokes.map(joke => {
+            return (
+              <div key={joke.id} className='joke'>
+                <p>{joke.setup}</p>  
+                <p>{joke.punchline}</p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Users;
